157e48cf01062cf4bd163dda61d00b0f26e51eb7
Aleksey <aleshabelov2001@mail.ru>
2022-08-30 16:28:00 +0400

    socket test

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 
 import socket
+from views import *
+
+URLS = {
+    '/': index,
+    '/blog': blog
+}
+
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return (method, url)
+
+
+def generate_headers(method, url):
+    if not method == 'GET':
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+    if not url in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>404</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Not allowed</p>'
+    return '<h1>{}</h1>'.format(URLS[url]())
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+    return (headers+body).encode()
 
 
 def run():
@@ -13,9 +49,12 @@ def run():
         request = client_socket.recv(1024)
         print(request)
         print()
+
+        print()
         print(addr)
 
-        client_socket.sendall("Hello world".encode())
+        response = generate_response(request.decode('utf-8'))
+        client_socket.sendall(response)
         client_socket.close()
 
 
